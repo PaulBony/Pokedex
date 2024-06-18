2420170f75b86d21f02a1ebad9a34df354e0bbce
aboni <pauloo.bonidon@gmail.com>
2024-06-18 10:56:18 +0200

    exercice 3

--- src/conponents/PokemonCard.jsx
+++ src/conponents/PokemonCard.jsx
@@ -1,11 +1,26 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[1];
+  console.log(pokemon);
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt=""
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc != null ? (
+        <img src={pokemon.imgSrc} alt="" />
+      ) : (
+        <p>???</p>
+      )}
+
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
