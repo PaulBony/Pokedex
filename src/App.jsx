d49c5fc636938725969af7ba3acb26a707ffad19
aboni <pauloo.bonidon@gmail.com>
2024-06-18 11:56:41 +0200

    props

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import PokemonCard from "./conponents/PokemonCard";
 import "./App.css";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
   return (
     <>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[1]} />
+      <PokemonCard pokemon={pokemonList[0]} />
     </>
   );
 }
--- src/conponents/PokemonCard.jsx
+++ src/conponents/PokemonCard.jsx
@@ -1,26 +1,15 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+function PokemonCard(props) {
+  console.log(props);
 
-function PokemonCard() {
-  const pokemon = pokemonList[1];
-  console.log(pokemon);
   return (
     <figure>
-      {pokemon.imgSrc != null ? (
-        <img src={pokemon.imgSrc} alt="" />
+      {props.pokemon.imgSrc != null ? (
+        <img src={props.pokemon.imgSrc} alt="" />
       ) : (
         <p>???</p>
       )}
 
-      <figcaption>{pokemon.name}</figcaption>
+      <figcaption>{props.pokemon.name}</figcaption>
     </figure>
   );
 }
